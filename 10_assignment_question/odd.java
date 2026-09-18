5daa007a971da354b798958eeeed516c5fc972c0
Gouthukutty <151035654+Gouthukutty@users.noreply.github.com>
2026-09-18 04:00:30 +0000

    Add star pattern assignment

---
+++ 10_assignment_question/odd.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void check(int x) {
+
+        if (x % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
